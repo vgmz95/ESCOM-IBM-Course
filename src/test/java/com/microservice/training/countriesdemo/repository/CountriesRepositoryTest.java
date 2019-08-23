6efbd2c238fca4524f097dd7b6e309e9552fb383
Rodrigo Bautista <rodbautista711@gmail.com>
2019-08-23 09:03:26 -0500

    Testing the findById feature for JpaRepository.

---
+++ src/test/java/com/microservice/training/countriesdemo/repository/CountriesRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.microservice.training.countriesdemo.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.microservice.training.countriesdemo.model.Continent;
+import com.microservice.training.countriesdemo.model.Country;
+import com.microservice.training.countriesdemo.model.entity.CountryEntity;
+import com.microservice.training.countriesdemo.repository.api.CountryJpaRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class CountriesRepositoryTest {
+
+	@Autowired
+	CountryJpaRepository mongoRepository;
+
+	@Before
+	public void setUp() {
+		assertNotNull(mongoRepository);
+	}
+
+	@Test
+	public void findById() {
+
+		log.info("Testing Mysql JPA repository findById feature");
+
+		Integer countryId = 169;
+		Optional<CountryEntity> countryOpt = mongoRepository.findById(countryId);
+
+		if (countryOpt.isPresent()) {
+			CountryEntity country = countryOpt.get();
+
+			assertThat(country.getContinent()).isEqualToIgnoringCase(Continent.NORTH_AMERICA.getContinentName());
+			assertThat(country.getName()).isEqualToIgnoringCase("mexico");
+
+			log.info("Country found: {}", country);
+		} else {
+			log.error("COUNTRY WAS NOT FOUND FOR ID: {}", countryId);
+		}
+
+	}
+
+}
